732bfa320f27536241dbbf9fb57c784a96845b61
jmjuanes <josemijuanes@gmail.com>
2016-06-19 18:36:45 +0200

    index.js: first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//Exports to node
+module.exports =
+{
+	//Read a json file
+	read: require('./lib/read.js').read,
+
+	//Read a json file Sync
+	readSync: require('./lib/read.js').readSync,
+
+	//Write to a json file
+	write: require('./lib/write.js').write,
+
+	//Write to a json file sync
+	writeSync: require('./lib/write.js').writeSync,
+
+	//Extend a json object
+	extend: require('./lib/extend.js').extend
+};
